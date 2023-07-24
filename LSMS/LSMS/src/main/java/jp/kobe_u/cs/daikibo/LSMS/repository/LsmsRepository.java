b2dccc74ead4f5142ec9f04e1fbbd985ad5807cf
000-mike-000 <230t25t@gsuite.kobe-u.ac.jp>
2023-07-24 12:07:40 +0900

    エラー修正(２)

--- LSMS/LSMS/src/main/java/jp/kobe_u/cs/daikibo/LSMS/repository/LsmsRepository.java
+++ LSMS/LSMS/src/main/java/jp/kobe_u/cs/daikibo/LSMS/repository/LsmsRepository.java
@@ -1,5 +1,9 @@
+package jp.kobe_u.cs.daikibo.LSMS.repository;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import jp.kobe_u.cs.daikibo.LSMS.entity.Lsms;
 
 @Repository
 
