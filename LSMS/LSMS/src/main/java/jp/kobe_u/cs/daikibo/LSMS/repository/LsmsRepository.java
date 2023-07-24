ee122c784e7933168ff7ab5968c17e98845554bc
000-mike-000 <230t25t@gsuite.kobe-u.ac.jp>
2023-07-24 10:41:57 +0900

    'repository作成'

---
+++ LSMS/LSMS/src/main/java/jp/kobe_u/cs/daikibo/LSMS/repository/LsmsRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface LsmsRepository extends CrudRepository<Lsms, Long>{
+
+}
