450841504a23a1038f31989138d23a551b797338
Wasser-san <hiki283@gmail.com>
2023-07-24 09:51:33 +0900

    spring boot 追加

---
+++ LSMS/LSMS/src/main/java/jp/kobe_u/cs/daikibo/LSMS/LsmsApplication.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.LSMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsmsApplication.class, args);
+	}
+
+}
